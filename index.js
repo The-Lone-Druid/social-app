b304cf7a5c329843a5ab549d50300f263a543d0d
Zahid Shaikh <zahid.786shaikh@gmail.com>
2023-01-17 21:10:43 +0530

    Couple of apis added Swagger dos updated

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
 const express = require("express");
 const format = require("date-format");
-const app = express();
-
+const fileUpload = require("express-fileupload");
 // swagger docs related
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
+const app = express();
 const swaggerDocument = YAML.load("./swagger.yaml");
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 const PORT = process.env.PORT || 4000;
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(fileUpload());
+
+let courses = [
+  {
+    id: "11",
+    name: "Learn Reactjs",
+    price: 299
+  },
+  {
+    id: "12",
+    name: "Learn Angular",
+    price: 499
+  },
+  {
+    id: "13",
+    name: "Learn Django",
+    price: 999
+  }
+];
+
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello from LCO</h1>");
+  res.send(
+    "<h1>Welcome to Social App</h1><h4>Visit API Docs on /api-docs</h4>"
+  );
 });
 
-app.get("/api/v1/instagram", (req, res) => {
-  const instaSocial = {
-    username: "thelonedruid56",
-    folowers: 66,
-    follows: 70,
-    date: format.asString("dd[MM] - hh:mm:ss", new Date())
-  };
-
-  res.status(200).json(instaSocial);
+app.get("/api/v1/lco", (req, res) => {
+  res.send("Hello from LCO");
 });
 
-app.get("/api/v1/facebook", (req, res) => {
-  const instaSocial = {
-    username: "thelonedruid56",
-    folowers: 88,
-    follows: 10,
-    date: format.asString("dd[MM] - hh:mm:ss", new Date())
-  };
-
-  res.status(200).json(instaSocial);
+app.get("/api/v1/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.get("/api/v1/linkedin", (req, res) => {
-  const instaSocial = {
-    username: "thelonedruid56",
-    folowers: 800,
-    follows: 80,
-    date: new Date()
-  };
-
-  res.status(200).json(instaSocial);
+app.get("/api/v1/courses/:courseId", (req, res) => {
+  const myCourse = courses.find((course) => course.id === req.params.courseId);
+  res.json(myCourse);
 });
 
-app.get("/api/v1/:token", (req, res) => {
-  console.log(req.params.token);
-  res.status(200).json({ param: req.params.token });
+app.post("/api/v1/add-course", (req, res) => {
+  const id = (parseInt(courses.reverse()[0].id) + 1).toString();
+  courses.push({ id, ...req.body });
+  res.json({ id, ...req.body });
+});
+
+app.get("/api/v1/coursequery", (req, res) => {
+  const location = req.query.location;
+  const device = req.query.device;
+
+  res.send({
+    location,
+    device
+  });
+});
+
+app.post("/api/v1/course-upload", (req, res) => {
+  const file = req.files.file;
+  let path = __dirname + "/assets/images/" + Date.now() + ".jpg";
+  console.log(req.headers);
+  file.mv(path, (err) => {
+    res.send(true);
+  });
 });
 
 app.listen(PORT, () => {
